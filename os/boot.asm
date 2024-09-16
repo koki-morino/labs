dd5f3ff698bb44cf6f397f7a88795c91cc339e5c
Koki Morino <koki.morino@outlook.jp>
2024-09-16 20:00:18 +0900

    Copy some examples

---
+++ os/boot.asm
@@ -0,0 +1,16 @@
+; Bootloader
+[BITS 16]
+
+ORG 0x7C00
+
+BOOT:
+    CLI ; Prevent all IRQs.
+    HLT ; Wait for next IRQ.
+
+; Assembler replaces $$ with an address of first instruction.
+; Assembler replaces $ with an address of current instruction.
+; ($ - $$) is to be 2 bits.
+; This results in putting 508 bits of zeros.
+TIMES 510 - ($ - $$) DB 0
+
+DW 0xAA55 ; Put word size data (2 bytes).
