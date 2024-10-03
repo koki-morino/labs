628d12323a8543e3372826e24f30ede2d0cf81c9
Koki Morino <koki.morino@outlook.jp>
2024-10-03 17:02:24 +0900

    Remove directories

--- cs/Common/Constants.cs
+++
@@ -1,6 +0,0 @@
-namespace Common;
-
-public static class Constants
-{
-    public const string Title = "XUI";
-}
--- cs/Desktop/ViewModels/MainWindowViewModel.cs
+++
@@ -1,9 +0,0 @@
-using Common;
-
-namespace Desktop.ViewModels;
-
-public partial class MainWindowViewModel : ViewModelBase
-{
-    public string Title => Constants.Title;
-    public string Greeting => "Welcome to Avalonia!";
-}
