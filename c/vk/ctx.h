16f4af58608185f5173cd927376e9ab768906f39
Koki Morino <koki.morino@outlook.jp>
2024-10-10 14:47:57 +0000

    Change coding style

--- c/vk/ctx.h
+++ c/vk/ctx.h
@@ -16,24 +16,24 @@ extern "C"
 
 typedef struct GPU_ctx
 {
-        VkInstance instance;
-        VkPhysicalDevice pdev;
-        VkDevice ldev;
-        uint32_t compute_queue_family_index;
-        VkQueue compute_queue;
-        VkDescriptorSetLayout descriptor_set_layout;
-        VkPipelineLayout pipeline_layout;
-        VkShaderModule shader_module;
-        VkPipeline pipeline;
-        VkDescriptorPool descriptor_pool;
-        VkDescriptorSet descriptor_set;
-        VkDeviceMemory input_memory;
-        VkBuffer input_buffer;
-        VkDeviceMemory output_memory;
-        VkBuffer output_buffer;
-        VkCommandPool compute_command_pool;
-        VkCommandBuffer command_buffer;
-        VkFence fence;
+    VkInstance instance;
+    VkPhysicalDevice pdev;
+    VkDevice ldev;
+    uint32_t compute_queue_family_index;
+    VkQueue compute_queue;
+    VkDescriptorSetLayout descriptor_set_layout;
+    VkPipelineLayout pipeline_layout;
+    VkShaderModule shader_module;
+    VkPipeline pipeline;
+    VkDescriptorPool descriptor_pool;
+    VkDescriptorSet descriptor_set;
+    VkDeviceMemory input_memory;
+    VkBuffer input_buffer;
+    VkDeviceMemory output_memory;
+    VkBuffer output_buffer;
+    VkCommandPool compute_command_pool;
+    VkCommandBuffer command_buffer;
+    VkFence fence;
 } GPU_ctx;
 
 int8_t GPU_ctx_init(GPU_ctx *ctx);
